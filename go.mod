245f3b74d0b04bd217997616f46b896587d87164
beardfriend <beardfriend21@gmail.com>
2022-07-27 01:00:54 +0900

    search query sample

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module goyo
 go 1.18
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/joho/godotenv v1.4.0
 	github.com/labstack/echo/v4 v4.7.2
 	gorm.io/driver/mysql v1.3.5
@@ -10,6 +11,7 @@ require (
 )
 
 require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
--- modules/academy/academy_controller.go
+++ modules/academy/academy_controller.go
@@ -1,17 +1,42 @@
 package academy
 
 import (
-	"goyo/models"
-	"goyo/server/mariadb"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/labstack/echo/v4"
 )
 
 type AcademyController struct{}
 
 func (AcademyController) CrawlNaver(c echo.Context) error {
-	var result []models.AdminiStrationDivision
-	mariadb.GetInstance().Raw(`SELECT si_gun_gu FROM administration_division WHERE si_do='서울'`).Find(&result)
+	search := "서울시 항동 요가학원"
+	encodedUrl := url.QueryEscape(search)
+	url := fmt.Sprintf("https://pcmap.place.naver.com/place/list?query=%s", encodedUrl)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
 
-	return c.JSON(200, result)
+	html, _ := goquery.NewDocumentFromReader(resp.Body)
+	index := strings.Index(html.Text(), "{\"$ROOT_QUERY.places")
+	index2 := strings.Index(html.Text(), "window.__PLACE_STATE__")
+	fmt.Println(index, index2)
+	str := strings.Trim(html.Text()[index:index2], " ")
+	dd := str[:len(str)-2]
+	var ss map[string]interface{}
+
+	json.Unmarshal([]byte(dd), &ss)
+	// var result []models.AdminiStrationDivision
+	// mariadb.GetInstance().Raw(`SELECT si_gun_gu FROM administration_division WHERE si_do='서울'`).Find(&result)
+	firstQuery := fmt.Sprintf("$ROOT_QUERY.places({\"input\":{\"adult\":false,\"deviceType\":\"pcmap\",\"display\":50,\"query\":\"%s\",\"queryRank\":\"\",\"spq\":false,\"start\":1}})", search)
+	ff := ss[firstQuery].(map[string]interface{})
+	ff2 := ff["items"]
+	return c.JSON(200, ff2)
 }
