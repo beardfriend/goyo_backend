ad86aa3e0006cb646734105fc379a47d7a6a7810
beardfriend <beardfriend21@gmail.com>
2022-08-11 21:55:59 +0900

    redis module

---
+++ server/redis/index.go
@@ -0,0 +1,29 @@
+package redis
+
+import (
+	"fmt"
+	"sync"
+
+	"goyo/libs"
+
+	"github.com/go-redis/redis/v9"
+)
+
+var (
+	instance  *redis.Client
+	onceRedis sync.Once
+)
+
+func GetInstance() *redis.Client {
+	onceRedis.Do(func() {
+		addr := fmt.Sprintf("%s:%s", libs.ENV.Redis.Host, libs.ENV.Redis.Port)
+
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     addr,
+			Password: "",
+			DB:       0,
+		})
+		instance = rdb
+	})
+	return instance
+}
